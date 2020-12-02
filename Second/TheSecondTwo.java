2cf562fe22dae7cc91b9f40c0ca64f90fda3c9f7
tucksravin <43099664+tucksravin@users.noreply.github.com>
2020-12-02 12:12:39 -0800

    SecondTwo works

--- Second/TheSecondTwo.java
+++ Second/TheSecondTwo.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.File;
 import java.io.IOException;
 
-public class TheSecondOne{
+public class TheSecondTwo{
 
   public static void  main(String args[]){
 
@@ -20,24 +20,23 @@ public class TheSecondOne{
 
         //System.out.println(curr);
 
-        int floor = Integer.parseInt(curr.substring(0, curr.indexOf("-")));
-        //System.out.println("Floor: " + floor);
+        int posOne = Integer.parseInt(curr.substring(0, curr.indexOf("-")))-1;
+
 
         //System.out.println(curr);
-        int ceiling = Integer.parseInt(curr.substring(curr.indexOf("-")+1, curr.indexOf(' ')));
-          //System.out.println("Ceiling: " + ceiling);
+        int posTwo = Integer.parseInt(curr.substring(curr.indexOf("-")+1, curr.indexOf(' ')))-1;
+          System.out.println("Position two: " + posTwo);
         char letter = curr.charAt(curr.indexOf(" ")+1);
         char[] pw = curr.substring(curr.indexOf(":")+2,curr.length()).toCharArray();
 
-        int letterCount = 0;
+//System.out.println("Position one: " + posOne);
+//System.out.println(pw[posOne]);
 
-        for(int i = 0 ; i < pw.length ; i++)
-        {
-          if(pw[i]==letter)
-            letterCount++;
-        }
+//System.out.println("Position twp: " + posTwo);
+//System.out.println(pw[posTwo]);
 
-        if(letterCount>=floor&&letterCount<=ceiling)
+
+        if((letter==pw[posOne])^(letter==pw[posTwo]))
           valid++;
 
         total++;
